5552f080161dc9f52ad9192999764702000406ac
Keunho Byeon <bkh5922a@gmail.com>
2021-12-01 19:47:08 +0900

    Modify tfrecord_to_csv.py

--- tools/tfrecord_to_csv.py
+++ tools/tfrecord_to_csv.py
@@ -1,10 +1,8 @@
-import multiprocessing
 import os
 
 import numpy as np
 import requests
 import tensorflow as tf
-from joblib import Parallel, delayed
 from tqdm import tqdm
 
 data_dir = os.path.expanduser('~/data/yt8m/video')
@@ -12,8 +10,7 @@ output_dir = os.path.expanduser('~/data/yt8m/annotation/')
 
 
 def read_tfrecords_and_make_csv(tfrecord_filename):
-    examples = []
-    error_ptr = 0
+    succeed, error = 0, 0
 
     ext = os.path.splitext(tfrecord_filename)[-1]
     output_filename = os.path.basename(tfrecord_filename).replace(ext, '.csv')
@@ -34,16 +31,15 @@ def read_tfrecords_and_make_csv(tfrecord_filename):
                 id_code = id_response.text.split('\",\"')[1].split('\");')[0]
                 labels = np.array(feature['labels'].int64_list.value).astype(str)
                 line = '{},{}'.format(str(id_code), '|'.join(labels))
-                wf.write(line + '\n')  # examples.append(line)
+                wf.write(line + '\n')
+                succeed += 1
             else:
-                error_ptr += 1
+                raise AssertionError
         except Exception as e:
-            continue
-
+            error += 1
     wf.close()
 
-    # print(error_ptr)
-    return examples
+    return succeed, error
 
 
 if __name__ == '__main__':
@@ -54,4 +50,12 @@ if __name__ == '__main__':
             if ext == '.tfrecord':
                 tfrecord_paths.append(os.path.join(path, filename))
 
-    Parallel(n_jobs=multiprocessing.cpu_count())(delayed(read_tfrecords_and_make_csv)(tfrecord_path) for tfrecord_path in tqdm(tfrecord_paths))
+    total_succeed, total_error = 0, 0
+    for tfrecord_path in tqdm(tfrecord_paths):
+        succeed, error = read_tfrecords_and_make_csv(tfrecord_path)
+        total_succeed += succeed
+        total_error += error
+
+    print('Succeed: {}\nError: {}'.format(total_succeed, total_error))
+
+    # Parallel(n_jobs=multiprocessing.cpu_count())(delayed(read_tfrecords_and_make_csv)(tfrecord_path) for tfrecord_path in tqdm(tfrecord_paths))
