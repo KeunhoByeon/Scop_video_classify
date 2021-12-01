7d6d60c3988b693feb6349e5a5d21101a4a09028
KeunhoByeon <bkh5922a@gmail.com>
2021-12-01 20:13:50 +0900

    multiprocessing handling

--- tools/tfrecord_to_csv.py
+++ tools/tfrecord_to_csv.py
@@ -1,4 +1,6 @@
+import multiprocessing
 import os
+from concurrent.futures import ThreadPoolExecutor
 
 import numpy as np
 import requests
@@ -9,6 +11,17 @@ data_dir = os.path.expanduser('~/data/yt8m/video')
 output_dir = os.path.expanduser('~/data/yt8m/annotation/')
 
 
+def get_id_from_url(id_url):
+    try:
+        id_response = requests.get(id_url)
+        if id_response.status_code == 200:
+            return id_response.text.split('\",\"')[1].split('\");')[0]
+        else:
+            raise AssertionError
+    except Exception as e:
+        return None
+
+
 def read_tfrecords_and_make_csv(tfrecord_filename):
     succeed, error = 0, 0
 
@@ -16,28 +29,36 @@ def read_tfrecords_and_make_csv(tfrecord_filename):
     output_filename = os.path.basename(tfrecord_filename).replace(ext, '.csv')
     output_path = os.path.join(output_dir, output_filename)
 
-    wf = open(output_path, 'w')
-
+    temp_id_list, id_url_list, labels_list = [], [], []
     record_iterator = tf.compat.v1.io.tf_record_iterator(path=tfrecord_filename)
     for string_record in record_iterator:
         try:
             example = tf.train.Example()
             example.ParseFromString(string_record)
             feature = example.features.feature
-            id = feature['id'].bytes_list.value[0].decode('UTF-8')
-            id_url = 'http://data.yt8m.org/2/j/i/{}/{}.js'.format(id[:2], id)
-            id_response = requests.get(id_url)
-            if id_response.status_code == 200:
-                id_code = id_response.text.split('\",\"')[1].split('\");')[0]
-                labels = np.array(feature['labels'].int64_list.value).astype(str)
-                line = '{},{}'.format(str(id_code), '|'.join(labels))
-                wf.write(line + '\n')
-                succeed += 1
-            else:
-                raise AssertionError
+            temp_id = feature['id'].bytes_list.value[0].decode('UTF-8')
+            id_url = 'http://data.yt8m.org/2/j/i/{}/{}.js'.format(temp_id[:2], temp_id)
+            labels = np.array(feature['labels'].int64_list.value).astype(str)
+
+            temp_id_list.append(temp_id)
+            id_url_list.append(id_url)
+            labels_list.append(labels)
+
         except Exception as e:
             error += 1
-    wf.close()
+
+    max_workers = multiprocessing.cpu_count()
+    with ThreadPoolExecutor(max_workers=max_workers) as pool:
+        id_list = list(pool.map(get_id_from_url, id_url_list))
+
+    with open(output_path, 'w') as wf:
+        for temp_id, id, labels in zip(temp_id_list, id_list, labels_list):
+            if id is None:
+                error += 1
+                continue
+
+            wf.write('{},{},{}\n'.format(str(temp_id), str(id), '|'.join(labels)))
+            succeed += 1
 
     return succeed, error
 
@@ -57,5 +78,3 @@ if __name__ == '__main__':
         total_error += error
 
     print('Succeed: {}\nError: {}'.format(total_succeed, total_error))
-
-    # Parallel(n_jobs=multiprocessing.cpu_count())(delayed(read_tfrecords_and_make_csv)(tfrecord_path) for tfrecord_path in tqdm(tfrecord_paths))
